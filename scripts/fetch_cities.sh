9fef9bbff6d0612851d6cc134ba39a9a26be4588
edouard <edouard.argenson@datapred.com>
2022-02-20 00:53:09 +0100

    create table with alembic instead of sql script

--- scripts/fetch_cities.sh
+++ scripts/fetch_cities.sh
@@ -4,6 +4,8 @@ set -a
 source config/.env
 set a+
 
+poetry run alembic upgrade head
+
 # TODO add this as script argument
 #poetry run python ceae/pipeline.py
 poetry run python ceae/sql_pipeline.py
